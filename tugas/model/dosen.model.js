a19ed854ca5829920bc00aa3872d4d7305ae1054
evan <mharisevan@gmail.com>
2026-05-21 15:44:52 +0700

    Menghapus file yang tidak sengaja ter-push

--- tugas/model/dosen.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const dosenSchema = new mongoose.Schema({
-  _id: { type: String, required: true },
-  nip: { type: String, required: true, unique: true },
-  nama: { type: String, required: true }
-}, { timestamps: true });
-
-module.exports = mongoose.model('Dosen', dosenSchema);
--- tugas/model/matakuliah.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const matakuliahSchema = new mongoose.Schema({
-  _id: { type: String, required: true },
-  nama_mk: { type: String, required: true },
-  pengajar_id: { type: String, ref: 'Dosen', required: true }
-}, { timestamps: true });
-
-module.exports = mongoose.model('Matakuliah', matakuliahSchema);
