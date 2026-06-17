1f98dd92774b1395af4b736569cbbe4224f48713
evan <mharisevan@gmail.com>
2026-06-17 09:29:17 +0700

    final fix

--- app.js
+++ app.js
@@ -3,19 +3,31 @@ const app = express();
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const dotenv = require('dotenv');
+const path = require('path'); 
+
 dotenv.config();
 
-mongoose.connect(process.env.DB)
-  .then(() => {
-      app.listen(3000);
-      console.log('Berjalan di port 3000')
-  })
-  .catch(err => console.error('Gagal terhubung:', err));
+const connectDB = async () => {
+    if (mongoose.connection.readyState >= 1) return;
+    try {
+        await mongoose.connect(process.env.DB);
+        console.log('MongoDB Atlas Terhubung');
+    } catch (err) {
+        console.error('Gagal terhubung:', err);
+    }
+};
+
+app.use(async (req, res, next) => {
+    await connectDB();
+    next();
+});
 
 app.use(methodOverride('_method'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views')); 
 
 const mahasiswaRoutes = require('./routes/mahasiswa.js');
 const dosenRoutes = require('./routes/dosen.js');
@@ -27,5 +39,11 @@ app.use('/dosen', dosenRoutes);
 app.use('/matakuliah', matakuliahRoutes);
 
 app.get('/', indexController.renderDashboard);
+
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(3000, () => {
+        console.log('Berjalan di port 3000');
+    });
+}
 
 module.exports = app;
